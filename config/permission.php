7edaf9d9f0c68ecfea9c4c45cceecc680b4fd1ef
zl20117 <287640817@qq.com>
2019-08-14 16:29:02 +0800

    feat:修改

--- config/permission.php
+++ config/permission.php
@@ -2,9 +2,8 @@
 
 return [
     'models' => [
-
         /*
-         * When using the "HasPermissions" trait from this package, we need to know which
+         * When is using the "HasPermissions" trait from this package, we need to know which
          * Eloquent model should be used to retrieve your permissions. Of course, it
          * is often just the "Permission" model but you may use whatever you like.
          *
